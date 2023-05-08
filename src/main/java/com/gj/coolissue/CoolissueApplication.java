1f974ae4bf750aca7284ee2f55d3489d120de523
gajun_choi <5tmdhkfem@naver.com>
2023-05-08 19:50:58 +0900

    first commit

---
+++ src/main/java/com/gj/coolissue/CoolissueApplication.java
@@ -0,0 +1,13 @@
+package com.gj.coolissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolissueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolissueApplication.class, args);
+	}
+
+}
